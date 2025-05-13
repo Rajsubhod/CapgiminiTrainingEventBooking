bd1ce904aef0a695f0e4de28fcb2ee9a76d85693
Ras-hi <iemrashicse@gmail.com>
2025-05-13 15:15:39 +0530

    load &save events

--- src/main/java/com/captraining/service/EventBookingSystem.java
+++ src/main/java/com/captraining/service/EventBookingSystem.java
@@ -1,12 +1,17 @@
 package com.captraining.service;
 
-import com.captraining.entity.Attendee;
-import com.captraining.entity.Event;
-import com.captraining.entity.Organizer;
-import com.captraining.entity.Ticket;
-
+import java.awt.Event;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import src.main.java.com.captraining.entity.Attendee;
+import src.main.java.com.captraining.entity.Organizer;
+import src.main.java.com.captraining.entity.Ticket;
 
 public class EventBookingSystem {
 
@@ -24,6 +29,21 @@ public class EventBookingSystem {
     public void addEvent(Event event) {
     	events.add(event);
     }
+    public void saveEvents() {
+    	try(ObjectOutputStream out =new ObjectOutputStream(new FileOutputStream("events.ser")))	{
+    		out.writeObject(events);
+    	}
+    	catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    public void loadEvents() {
+    	try(ObjectInputStream in =new ObjectInputStream(new FileInputStream("events.ser"))){
+    		events=(List<jdk.jfr.Event>)in.readObject();
+    	}
+    	catch(Exception e) {
+    		System.out.println("no saved events found.");
+    	}
+    }
     
-
 }
