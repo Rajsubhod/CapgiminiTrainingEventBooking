d1890bf5a939afcceeb946c8ddb4c5a54582f8f3
Ras-hi <iemrashicse@gmail.com>
2025-05-13 14:16:52 +0530

    second commit

--- src/main/java/com/captraining/entity/Event.java
+++ src/main/java/com/captraining/entity/Event.java
@@ -1,4 +1,12 @@
 package com.captraining.entity;
 
 public class Event {
+	private String title;
+	private int availableTickets;
+	public Event(String title, int availableTickets) {
+		this.title=title;
+		this.availableTickets=availableTickets;
+	}
+	
+	
 }
